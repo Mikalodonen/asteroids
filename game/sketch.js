e249af780e2c1f2b44369c59c53ce8cccc998ff5
mikkel <mikkelandersen295@gmail.com>
2022-01-13 12:59:21 +0100

    addede function player

--- game/sketch.js
+++ game/sketch.js
@@ -4,9 +4,15 @@ function setup() {
 
 function draw() {
   background("darkgrey");
-  
+
  // Vi skal snart lvae noget
-  
+}
+function player(){
+
+  //Player starter i centrum
+  this.pos = createVector(height/2, width/2)
+    this.render = function() {
 
 
+    }
 }
